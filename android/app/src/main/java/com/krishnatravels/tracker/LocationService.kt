15f64f97b5195b72415c9395c935da2f16e2edd7
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-27 22:56:33 +0530

    Update LocationService.kt

--- android/app/src/main/java/com/krishnatravels/tracker/LocationService.kt
+++ android/app/src/main/java/com/krishnatravels/tracker/LocationService.kt
@@ -46,7 +46,11 @@ class LocationService : Service() {
             .setSmallIcon(android.R.drawable.ic_menu_mylocation)
             .build()
         
-        startForeground(1, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(1, notification, android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION)
+        } else {
+            startForeground(1, notification)
+        }
         requestLocationUpdates()
         
         return START_STICKY
