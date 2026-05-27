f83b9495ebe5b16f2660f3ac157df782e982cc8d
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-27 11:33:30 +0530

    Create RouteMapActivity.kt

---
+++ android/app/src/main/java/com/krishnatravels/tracker/RouteMapActivity.kt
@@ -0,0 +1,11 @@
+package com.krishnatravels.tracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RouteMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route_map)
+    }
+}
