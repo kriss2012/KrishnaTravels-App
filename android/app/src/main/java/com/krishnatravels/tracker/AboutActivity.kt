cc6d45143910b7972ca9b39f598ee2ccbdb4d71d
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-28 11:12:27 +0530

    Update AboutActivity.kt

--- android/app/src/main/java/com/krishnatravels/tracker/AboutActivity.kt
+++ android/app/src/main/java/com/krishnatravels/tracker/AboutActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        ThemeManager.applyTheme(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
     }
