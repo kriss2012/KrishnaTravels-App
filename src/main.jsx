a8d7912149232d8d625405b4332959423abec8dc
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:35:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/**
+ * File: main.jsx
+ * Date: 2026-05-29
+ * #by Kiri Team
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
