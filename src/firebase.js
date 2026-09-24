141aba4e4ce89ffd7d45a36b15a1c1be50aeb909
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-09-24 10:54:06 +0530

    z

--- src/firebase.js
+++ src/firebase.js
@@ -12,14 +12,14 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDs0dIlUJ-3eLTOxiA5QpzLEUfZ5BOVraw",
-    authDomain: "pachora-jalgaon-tracker.firebaseapp.com",
-    databaseURL: "https://pachora-jalgaon-tracker-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "pachora-jalgaon-tracker",
-    storageBucket: "pachora-jalgaon-tracker.firebasestorage.app",
-    messagingSenderId: "934921741539",
-    appId: "1:934921741539:web:6e9dd157721059d097d1d6",
-    measurementId: "G-CC76V337PC"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "pachora-jalgaon-tracker.firebaseapp.com",
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://pachora-jalgaon-tracker-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "pachora-jalgaon-tracker",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "pachora-jalgaon-tracker.firebasestorage.app",
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "934921741539",
+    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:934921741539:web:6e9dd157721059d097d1d6",
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-CC76V337PC"
 };
 
 const app = initializeApp(firebaseConfig);
